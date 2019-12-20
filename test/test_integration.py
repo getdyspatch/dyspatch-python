3543eda695028f9ad9bebf0f7791a0a01b610aef
Colin Walker <colin@sendwithus.com>
2019-12-20 10:17:17 -0800

    Add integration flows

---
+++ test/test_integration.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+import unittest
+import os
+
+import dyspatch_client
+
+version = "application/vnd.dyspatch.2019.10+json"
+
+class TestDraftRead(unittest.TestCase):
+    """DraftRead unit test stubs"""
+
+    def setUp(self):
+        key = os.environ['DYSPATCH_API_KEY']
+        configuration = dyspatch_client.Configuration()
+        configuration.api_key['Authorization'] = f'Bearer {key}'
+        client = dyspatch_client.ApiClient(configuration)
+        self.api = dyspatch_client.api.templates_api.TemplatesApi(client)
+
+    def test_list_templates(self):
+        templates = self.api.get_template(version)
+        print(templates)
+
+        template = self.api.get_template_by_id(
+            templates.data[0].id,
+            "handlebars",
+            version,
+        )
+        print(template)
+
+
+if __name__ == '__main__':
+    unittest.main()
